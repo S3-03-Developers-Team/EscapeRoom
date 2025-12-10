ff2a20f02cbae8e3a4ca8e9416c0a74321393ed1
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-12-10 13:42:52 +0100

    feat: add DAO for player data management

--- S3.3-escape-room/src/main/java/org/s3team/Player/DAO/PlayerDAO.java
+++ S3.3-escape-room/src/main/java/org/s3team/Player/DAO/PlayerDAO.java
@@ -5,7 +5,7 @@ import org.s3team.common.dao.CrudDao;
 
 import java.util.Optional;
 
-public interface PlayerDao extends CrudDao {
+public interface PlayerDAO extends CrudDao {
     Optional<Player> findByEmail(String email);
 
     Optional<Player> findByName(String nickName);
