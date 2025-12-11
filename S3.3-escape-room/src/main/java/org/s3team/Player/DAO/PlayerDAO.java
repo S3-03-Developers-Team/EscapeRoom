aa3769653ed4888dcafd153dbe28ccd3bdc427e9
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-12-11 11:23:09 +0100

    fix: garantees that PlayerDao implements PLayer objects in methods crud

--- S3.3-escape-room/src/main/java/org/s3team/Player/DAO/PlayerDAO.java
+++ S3.3-escape-room/src/main/java/org/s3team/Player/DAO/PlayerDAO.java
@@ -5,7 +5,7 @@ import org.s3team.common.dao.CrudDao;
 
 import java.util.Optional;
 
-public interface PlayerDAO extends CrudDao {
+public interface PlayerDAO extends CrudDao<Player> {
     Optional<Player> findByEmail(String email);
 
     Optional<Player> findByName(String nickName);
