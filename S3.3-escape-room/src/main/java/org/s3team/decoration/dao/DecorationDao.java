1ada329812063bacd0f703b43bd744287fe45335
Spook242 <spook242@gmail.com>
2025-12-12 19:05:20 +0100

    feat: add findByRoom and updateRoom methods.

--- S3.3-escape-room/src/main/java/org/s3team/decoration/dao/DecorationDao.java
+++ S3.3-escape-room/src/main/java/org/s3team/decoration/dao/DecorationDao.java
@@ -3,8 +3,13 @@ package org.s3team.decoration.dao;
 import org.s3team.common.dao.CrudDao;
 import org.s3team.decoration.model.Decoration;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DecorationDao extends CrudDao<Decoration> {
     Optional<Decoration> findById(int id);
+
+    List<Decoration> findByRoomId(int roomId);
+
+    boolean updateRoom(int decorationId, int newRoomId);
 }
