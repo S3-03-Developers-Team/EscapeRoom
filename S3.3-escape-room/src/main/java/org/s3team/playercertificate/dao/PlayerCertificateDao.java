a2ffdbfab5a79ba7493167186f81af04294e6eb8
Cesar <ccasro@gmail.com>
2025-12-12 19:15:47 +0100

    feat(playercertificate): add DTO methods on interface for related info

--- S3.3-escape-room/src/main/java/org/s3team/playercertificate/dao/PlayerCertificateDao.java
+++ S3.3-escape-room/src/main/java/org/s3team/playercertificate/dao/PlayerCertificateDao.java
@@ -3,6 +3,7 @@ package org.s3team.playercertificate.dao;
 import org.s3team.Player.Model.Player;
 import org.s3team.certificate.model.Certificate;
 import org.s3team.common.valueobject.Id;
+import org.s3team.playercertificate.dto.PlayerCertificateInfo;
 import org.s3team.playercertificate.model.PlayerCertificate;
 import org.s3team.room.model.Room;
 
@@ -16,4 +17,7 @@ public interface PlayerCertificateDao {
     List<PlayerCertificate> findByRoom(Id<Room> roomId) throws SQLException;
     List<PlayerCertificate> findAll() throws SQLException;
     boolean exists(Id<Player> playerId, Id<Certificate> certificateId, Id<Room> roomId) throws SQLException;
+
+    List<PlayerCertificateInfo> findCertificatesByPlayerWithInfo(Id<Player> playerId) throws SQLException;
+    List<PlayerCertificateInfo> findCertificatesByRoomWithInfo(Id<Room> roomId) throws SQLException;
 }
