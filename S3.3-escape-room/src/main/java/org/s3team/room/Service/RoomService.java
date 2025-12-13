34ce9ef80fd293e05d6841606ad3bec7fe4003ba
jrffruit518-enrich <jrffruit518@gmail.com>
2025-12-13 17:58:19 +0100

    Dev room (#120)
    * feat: Room Module CRUD and Persistence"
    
    * feat: Room Module CRUD and Persistence"
    
    * Remove .idea folder from repository
    
    * fix: room y RoomDAOImp, use rehydrate in seveRoom
    
    * Ignore .idea/ and .iml files
    
    * Unified .gitignore: move to project root and ignore .idea/
    
    * Ignore .idea/ and other IDE files
    
    * Update .gitignore
    
    * refactor(room): Introduce DI for RoomDAO and manage connections locally
    
    * fix : update RoomService
    
    ---------
    
    Co-authored-by: ccasro <116024790+ccasro@users.noreply.github.com>

--- S3.3-escape-room/src/main/java/org/s3team/room/Service/RoomService.java
+++ S3.3-escape-room/src/main/java/org/s3team/room/Service/RoomService.java
@@ -1,54 +1,74 @@
 package org.s3team.room.Service;
 
+import org.s3team.Exceptions.RoomNotFoundException;
+import org.s3team.Exceptions.ThemeNotFoundException;
 import org.s3team.common.valueobject.*;
-import org.s3team.room.DAO.RoomDAOImp;
+import org.s3team.room.DAO.RoomDAO;
 import org.s3team.room.model.Room;
+import org.s3team.theme.dao.ThemeDao;
+import org.s3team.theme.model.Theme;
 
 import java.util.List;
 import java.util.Optional;
 
 public class RoomService {
-    private RoomDAOImp roomDAOImp;
+    private final RoomDAO roomDAO;
+    private final ThemeDao themeDao;
 
-    public RoomService(RoomDAOImp roomDAOImp) {
-        this.roomDAOImp = roomDAOImp;
+    public RoomService(RoomDAO roomDAO, ThemeDao themeDao) {
+        this.roomDAO = roomDAO;
+        this.themeDao = themeDao;
     }
 
     public Room save(Room room) {
-        return roomDAOImp.save(room);
-
+        Id<Theme> themeId = room.getThemeId();
+        themeDao.findById(themeId).orElseThrow(() ->
+                new ThemeNotFoundException("Theme with ID " + themeId.value() + " doesn't exist.")
+        );
+        return roomDAO.save(room);
     }
 
     public Room findById(Id<Room> id) {
-        Optional<Room> roomOptional = roomDAOImp.findById(id);
+        Optional<Room> roomOptional = roomDAO.findById(id);
 
         return roomOptional.orElseThrow(
-                () -> new RuntimeException("Room with ID " + id.value() + " not found.")
+                () -> new RoomNotFoundException("Room with ID " + id.value() + " not found.")
         );
     }
 
     public List<Room> findAll() {
-        return roomDAOImp.findAll();
+        return roomDAO.findAll();
     }
 
     public boolean update(Room room) {
-        if (roomDAOImp.findById(room.getRoomId()).isEmpty()) {
-            return false;
-        }
-        return roomDAOImp.update(room);
+
+        roomDAO.findById(room.getRoomId()).orElseThrow(
+                () -> new RoomNotFoundException("Cannot update. Room with ID " + room.getRoomId().value() + " not found.")
+        );
+
+        Id<Theme> themeId = room.getThemeId();
+        themeDao.findById(themeId).orElseThrow(() ->
+                new ThemeNotFoundException("Cannot update. Theme with ID " + themeId.value() + " doesn't exist.")
+        );
+
+        return roomDAO.update(room);
     }
 
     public boolean delete(Id<Room> id) {
-        return roomDAOImp.delete(id);
+
+        roomDAO.findById(id).orElseThrow(
+                () -> new RoomNotFoundException("Cannot delete. Room with ID " + id.value() + " not found.")
+        );
+
+        return roomDAO.delete(id);
     }
 
     public int count() {
-        return roomDAOImp.count();
-
+        return roomDAO.count();
     }
 
     public Price calculateTotalPrice() {
-        return roomDAOImp.calculateTotalPrice();
+        return roomDAO.calculateTotalPrice();
     }
 
 }
