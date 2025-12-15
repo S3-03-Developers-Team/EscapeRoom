b3a05aa6ed04707ce06eb4dbdc51bd4b3e01e87a
Cesar <ccasro@gmail.com>
2025-12-15 08:33:58 +0100

    feat(exceptions): add Client/Decoration/Ticket NotFoundExceptions

---
+++ S3.3-escape-room/src/main/java/org/s3team/Exceptions/ClueNotFoundException.java
@@ -0,0 +1,7 @@
+package org.s3team.Exceptions;
+
+public class ClueNotFoundException extends RuntimeException {
+    public ClueNotFoundException(String message) {
+        super(message);
+    }
+}
